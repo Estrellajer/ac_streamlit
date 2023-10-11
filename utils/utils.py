8e0169e69a038ebc8239c1066130385627bc2493
Estrellajer <101073612+Estrellajer@users.noreply.github.com>
2023-10-11 19:31:25 +0800

    Add files via upload

---
+++ utils/utils.py
@@ -0,0 +1,28 @@
+import datetime
+from enum import Enum
+import streamlit as st
+import requests
+import json
+
+HOST = "http://123.56.44.128:8001"
+
+def post(my_json: dict, path: str, token=None) -> dict:
+    # print(token)
+    if token is not None:
+        headers = {
+            # "Content-Type": "application/json",
+            "Authorization": token
+        }
+        response = requests.post(url=HOST + path, data=json.dumps(my_json), headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            st.error(response.text)
+        # return requests.post(url=HOST + path, data=json.dumps(my_json), headers=headers).json()
+    else:
+        # return requests.post(url=HOST + path, data=json.dumps(my_json)).json()
+        response = requests.post(url=HOST + path, data=json.dumps(my_json))
+        if response.status_code == 200:
+            return response.json()
+        else:
+            st.error(response.text)
